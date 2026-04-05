6d8cfb59d06f8f175ba34c7e9b717cb57684bb62
Ivan Guevara <ivanzaldivar16@gmail.com>
2026-04-05 10:40:10 -0600

    chore: set server port in Vite configuration for frontend and web applications

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), basicSsl()],
   server: {
     https: true,
+    port: 5174,
     proxy: {
       '/v1': {
         target: 'http://localhost:3000',
