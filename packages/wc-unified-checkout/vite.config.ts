d8914d790616e4e446a9060d54bc469677677be4
Ivan Guevara <ivanzaldivar16@gmail.com>
2026-04-07 12:59:54 -0600

    chore(packages/wc-unified-checkout): integrate @vitejs/plugin-basic-ssl for enhanced security and update index.html to use unified-checkout component

--- packages/wc-unified-checkout/vite.config.ts
+++ packages/wc-unified-checkout/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+  plugins: [basicSsl()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
